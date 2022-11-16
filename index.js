0db275faed36cddd43881cbe0602578f18486271
sohyeon-zoe-kim <hlna030838@gmail.com>
2022-11-17 00:55:48 +0900

    feat: favicon 추가 및 breadCrumbs 이벤트 함수화

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 window.onload = () => {
+  addBreadCrumbsEvent()
+}
+
+const addBreadCrumbsEvent = () => {
   const breadCrumbs = document.getElementsByClassName('back-button-home')
   breadCrumbs[0].addEventListener('click', () => {
     location.href = "/"
