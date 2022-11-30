5a5a86962553a012f63f52021afea9769e8e6054
sohyeon-zoe-kim <hlna030838@gmail.com>
2022-11-30 21:59:30 +0900

    feat: hover-effect-card 챌린지 ✨

---
+++ components/hover-effect-card/index.js
@@ -0,0 +1,23 @@
+const I_WANT_ROLLING_STONES_TIME = 10
+
+function createRollingStones() {
+  const img = document.createElement('img')
+  img.src = '../../assets/imgs/rolling-stones.png'
+  
+  const randomNumTop = Math.floor(Math.random() * 100) + 1
+  const randomNumLeft = Math.floor(Math.random() * 100) + 1
+  
+  img.style.top = `${randomNumTop}%`
+  img.style.left = `${randomNumLeft}%`
+  img.classList.add('hover-effect-card__img')
+
+  return img
+}
+
+function mounted() {
+  const div = document.getElementsByClassName('wrap')
+
+  for(let i=0; i<I_WANT_ROLLING_STONES_TIME; i++) {
+    div[0].append(createRollingStones())
+  }
+}
