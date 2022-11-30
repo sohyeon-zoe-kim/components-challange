c7af1f6b5c38a28852bbfd36aaabe54448f99f7a
sohyeon-zoe-kim <hlna030838@gmail.com>
2022-11-30 21:58:56 +0900

    feat: glowing-text 속도 조절 기능 버튼 추가

--- components/glowing-text/index.js
+++ components/glowing-text/index.js
@@ -1,15 +1,8 @@
-const ANIMATION_LOOP_TIME_INTERVAL = 1000
+const ANIMATION_LOOP_TIME_INTERVAL = 100
 const textList = document.getElementsByClassName('glowing-text__letter')
 let nowTextIndex = 0
 let timer = null
 const text = 'sorogi'
-
-function appendText(){
-  const div = document.getElementById('text-list')
-  for(let i=0; i<text.length; i++) {
-    div.innerHTML = `<span class="glowing-text__letter">${text.slice(i, i+1)}</span>`
-  }
-}
 
 function addAnimation(element) {
   element.classList.add('glowing-text__letter--blinking');
@@ -34,7 +27,7 @@ function loopAnimation() {
 }
 
 function startLoop() {
-  timer = setInterval(loopAnimation, 500)
+  timer = setInterval(loopAnimation, ANIMATION_LOOP_TIME_INTERVAL)
 }
 
 startLoop()
