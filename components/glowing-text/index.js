3420371d6d698418e669b4e4366994d5cb5d6967
sohyeon-zoe-kim <hlna030838@gmail.com>
2022-12-17 20:02:11 +0900

    feat: glowing-text 속도 조절 기능 추가(InProgress)

--- components/glowing-text/index.js
+++ components/glowing-text/index.js
@@ -1,22 +1,32 @@
-const ANIMATION_LOOP_TIME_INTERVAL = 100
-const textList = document.getElementsByClassName('glowing-text__letter')
+const ANIMATION_LOOP_TIME_INTERVAL = 300
 let nowTextIndex = 0
 let timer = null
-const text = 'sorogi'
+let textList = null
+const text = document.getElementsByClassName('glowing-text-input__box').value || 'SOROGI'
+
+const setGlowingText = () => {
+  const textList = document.getElementsByClassName('glowing-text-list')[0]
+  text.split('').forEach((letter) => {
+    const element = createElement('span')
+    element.innerHTML = letter
+    addClassName(element, 'glowing-text-list__letter')
+    textList.appendChild(element)
+  })
+}
 
 function addAnimation(element) {
-  element.classList.add('glowing-text__letter--blinking');
+  element.classList.add('glowing-text-list__letter--blinking');
 }
 
 function removeAllAnimation() {
   Array.from(textList).forEach(el => {
-    el.classList.remove('glowing-text__letter--blinking');
+    el.classList.remove('glowing-text-list__letter--blinking');
   })
 }
 
 function loopAnimation() {
   nowTextIndex++
-  if (nowTextIndex > 5) {
+  if (nowTextIndex >= text.length) {
     nowTextIndex = 0
     removeAllAnimation()
     addAnimation(Array.from(textList)[nowTextIndex])
@@ -30,4 +40,6 @@ function startLoop() {
   timer = setInterval(loopAnimation, ANIMATION_LOOP_TIME_INTERVAL)
 }
 
+setGlowingText()
+textList = document.getElementsByClassName('glowing-text-list__letter')
 startLoop()
