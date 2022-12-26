3fdc6e42ca7b65f6c67a25145e95694dcd619de5
sohyeon-zoe-kim <hlna030838@gmail.com>
2022-12-26 10:00:30 +0900

    feat: hover-effect-card 챌린지(1) ✨
    * UI 뼈대 생성

---
+++ components/hover-effect-card/index.js
@@ -0,0 +1,16 @@
+const cardList = document.getElementsByClassName('hover-effect-card')
+const cardItems = document.getElementsByClassName('hover-effect-card__item')
+
+{/* <div class="hover-effect-card__item">
+    <h1 class="hover-effect-card__item-title">Sorogi One</h1>
+    <p class="hover-effect-card__item-contents">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius optio nisi reprehenderit error culpa amet incidunt architecto numquam ad at dolorem odit maxime vel dolor mollitia sapiente, aperiam, quo quae?</p>
+    <button class="hover-effect-card__item-button">Read more</button>
+  </div> */}
+
+
+const addCard = (title, contents) => {
+  const card = createElement('div')
+  // const title
+}
+
+console.log(cardItems)
