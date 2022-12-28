9a4606a628b40083e213ec86dfb6ece552008fb7
sohyeon-zoe-kim <hlna030838@gmail.com>
2022-12-28 22:17:38 +0900

    feat: hover-effect-card 챌린지(2) ✨

--- components/hover-effect-card/index.js
+++ components/hover-effect-card/index.js
@@ -1,16 +1,39 @@
 const cardList = document.getElementsByClassName('hover-effect-card')
 const cardItems = document.getElementsByClassName('hover-effect-card__item')
+const CONTENTS = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius optio nisi reprehenderit error culpa amet incidunt architecto numquam ad at dolorem odit maxime vel dolor mollitia sapiente, aperiam, quo quae?'
 
 {/* <div class="hover-effect-card__item">
-    <h1 class="hover-effect-card__item-title">Sorogi One</h1>
-    <p class="hover-effect-card__item-contents">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius optio nisi reprehenderit error culpa amet incidunt architecto numquam ad at dolorem odit maxime vel dolor mollitia sapiente, aperiam, quo quae?</p>
-    <button class="hover-effect-card__item-button">Read more</button>
-  </div> */}
+      <div class="hover-effect-card__item-wrapper">
+        <h1 class="hover-effect-card__item-title">Sorogi One</h1>
+        <p class="hover-effect-card__item-contents">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius optio nisi reprehenderit error culpa amet incidunt architecto numquam ad at dolorem odit maxime vel dolor mollitia sapiente, aperiam, quo quae?</p>
+        <button class="hover-effect-card__item-button">Read more</button>
+      </div>
+    </div> */}
 
 
 const addCard = (title, contents) => {
-  const card = createElement('div')
-  // const title
+  const newCard = createElement('div')
+  const cardWrapper = createElement('div')
+  const cardTitle = createElement('h1')
+  const cardContents = createElement('p')
+  const cardButton = createElement('button')
+
+  addClassName(newCard, 'hover-effect-card__item hover-effect-card__item_pink')
+  addClassName(cardWrapper, 'hover-effect-card__item-wrapper')
+  addClassName(cardTitle, 'hover-effect-card__item-title')
+  addClassName(cardContents, 'hover-effect-card__item-contents')
+  addClassName(cardButton, 'hover-effect-card__item-button')
+
+  cardTitle.innerHTML = title
+  cardContents.innerHTML = contents
+  cardButton.innerHTML = 'Read more'
+
+  cardWrapper.append(cardTitle, cardContents, cardButton)
+  newCard.append(cardWrapper)
+  
+  cardList[0].append(newCard)
 }
 
-console.log(cardItems)
+addCard('Sorogi One', CONTENTS)
+addCard('Sorogi Two', CONTENTS)
+addCard('Sorogi Three', CONTENTS)
