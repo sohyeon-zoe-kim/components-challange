b0cdde51142f462b560b696bfec4f87ecac52e05
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-01-04 21:20:07 +0900

    feat: hover-effect-card 챌린지(3) ✨

--- components/hover-effect-card/index.js
+++ components/hover-effect-card/index.js
@@ -2,23 +2,14 @@ const cardList = document.getElementsByClassName('hover-effect-card')
 const cardItems = document.getElementsByClassName('hover-effect-card__item')
 const CONTENTS = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius optio nisi reprehenderit error culpa amet incidunt architecto numquam ad at dolorem odit maxime vel dolor mollitia sapiente, aperiam, quo quae?'
 
-{/* <div class="hover-effect-card__item">
-      <div class="hover-effect-card__item-wrapper">
-        <h1 class="hover-effect-card__item-title">Sorogi One</h1>
-        <p class="hover-effect-card__item-contents">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius optio nisi reprehenderit error culpa amet incidunt architecto numquam ad at dolorem odit maxime vel dolor mollitia sapiente, aperiam, quo quae?</p>
-        <button class="hover-effect-card__item-button">Read more</button>
-      </div>
-    </div> */}
-
-
-const addCard = (title, contents) => {
+const addCard = (title, contents, color) => {
   const newCard = createElement('div')
   const cardWrapper = createElement('div')
   const cardTitle = createElement('h1')
   const cardContents = createElement('p')
   const cardButton = createElement('button')
 
-  addClassName(newCard, 'hover-effect-card__item hover-effect-card__item_pink')
+  addClassName(newCard, `hover-effect-card__item hover-effect-card__item-${color}`)
   addClassName(cardWrapper, 'hover-effect-card__item-wrapper')
   addClassName(cardTitle, 'hover-effect-card__item-title')
   addClassName(cardContents, 'hover-effect-card__item-contents')
@@ -34,6 +25,6 @@ const addCard = (title, contents) => {
   cardList[0].append(newCard)
 }
 
-addCard('Sorogi One', CONTENTS)
-addCard('Sorogi Two', CONTENTS)
-addCard('Sorogi Three', CONTENTS)
+addCard('Sorogi One', CONTENTS, 'pink')
+addCard('Sorogi Two', CONTENTS, 'yellow')
+addCard('Sorogi Three', CONTENTS, 'skyblue')
