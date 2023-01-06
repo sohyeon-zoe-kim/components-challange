a067f21e691cecf50bb8b65d8f6f2f55eed47d15
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-01-06 18:05:28 +0900

    feat: accordion 챌린지(2) ✨

--- components/accordion/index.js
+++ components/accordion/index.js
@@ -1,7 +1,11 @@
 const accordionHeader = document.getElementsByClassName('accordion-item__header')[0]
 const accordionContents = document.getElementsByClassName('accordion-item__contents')[0]
+const plusIcon = document.getElementById('plus-icon')
+const minusIcon = document.getElementById('minus-icon')
 
 accordionHeader.addEventListener('click', () => {
   accordionContents.classList.toggle('hidden')
   accordionContents.classList.toggle('show')
+  plusIcon.classList.toggle('icon-hidden')
+  minusIcon.classList.toggle('icon-hidden')
 })
